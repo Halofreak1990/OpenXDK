f74ecc58dd4f861c3d50dc9381a957f26d07af7b
caustik <caustik>
2002-11-24 23:48:24 +0000

    Still merging

--- include/xboxkrnl/xbox.h
+++ include/xboxkrnl/xbox.h
@@ -12,56 +12,6 @@
 #ifndef XBOX_H
 #define XBOX_H
 
-NTSYSAPI VOID *AvGetSavedDataAddress;
-NTSYSAPI VOID *AvSendTVEncoderOption;
-NTSYSAPI VOID *AvSetDisplayMode;
-NTSYSAPI VOID *AvSetSavedDataAddress;
-NTSYSAPI VOID *FscGetCacheSize;
-NTSYSAPI VOID *FscInvalidateIdleBlocks;
-NTSYSAPI VOID *FscSetCacheSize;
-NTSYSAPI VOID *KdDebuggerEnabled;
-NTSYSAPI VOID *KdDebuggerNotPresent;
-NTSYSAPI VOID *KfRaiseIrql;
-NTSYSAPI VOID *KfLowerIrql;
-NTSYSAPI VOID *KiBugCheckData;
-NTSYSAPI VOID *KiUnlockDispatcherDatabase;
-NTSYSAPI VOID *LaunchDataPage;
-NTSYSAPI VOID *PhyGetLinkState;
-NTSYSAPI VOID *PhyInitialize;
-NTSYSAPI VOID *XboxEEPROMKey;
-NTSYSAPI VOID *XboxHardwareInfo;
-NTSYSAPI VOID *XboxHDKey;
-NTSYSAPI VOID *XboxKrnlVersion;
-NTSYSAPI VOID *XboxSignatureKey;
-NTSYSAPI VOID *XeImageFileName;
-NTSYSAPI VOID *XeLoadSection;
-NTSYSAPI VOID *XeUnloadSection;
-NTSYSAPI VOID *XcSHAInit;
-NTSYSAPI VOID *XcSHAUpdate;
-NTSYSAPI VOID *XcSHAFinal;
-NTSYSAPI VOID *XcRC4Key;
-NTSYSAPI VOID *XcRC4Crypt;
-NTSYSAPI VOID *XcHMAC;
-NTSYSAPI VOID *XcPKEncPublic;
-NTSYSAPI VOID *XcPKDecPrivate;
-NTSYSAPI VOID *XcPKGetKeyLen;
-NTSYSAPI VOID *XcVerifyPKCS1Signature;
-NTSYSAPI VOID *XcModExp;
-NTSYSAPI VOID *XcDESKeyParity;
-NTSYSAPI VOID *XcKeyTable;
-NTSYSAPI VOID *XcBlockCrypt;
-NTSYSAPI VOID *XcBlockCryptCBC;
-NTSYSAPI VOID *XcCryptService;
-NTSYSAPI VOID *XcUpdateCrypto;
-NTSYSAPI VOID *XboxLANKey;
-NTSYSAPI VOID *XboxAlternateSignatureKeys;
-NTSYSAPI VOID *XePublicKeyData;
-NTSYSAPI VOID *IdexChannelObject;
-NTSYSAPI VOID *xsnprintf;     // prefixed with "x" to avoid xlibc collisions
-NTSYSAPI VOID *xsprintf;      // ""
-NTSYSAPI VOID *xvsnprintf;    // ""
-NTSYSAPI VOID *xvsprintf;     // ""
-
 // ******************************************************************
 // * AvGetSavedDataAddress
 // ******************************************************************
--- src/xboxkrnl/xbox.c
+++ src/xboxkrnl/xbox.c
@@ -12,56 +12,6 @@
 #define _XBOXKRNL_INTERNAL_
 #include "xboxkrnl/xboxkrnl.h"
 
-NTSYSAPI VOID *AvGetSavedDataAddress = 0;
-NTSYSAPI VOID *AvSendTVEncoderOption = 0;
-NTSYSAPI VOID *AvSetDisplayMode = 0;
-NTSYSAPI VOID *AvSetSavedDataAddress = 0;
-NTSYSAPI VOID *FscGetCacheSize = 0;
-NTSYSAPI VOID *FscInvalidateIdleBlocks = 0;
-NTSYSAPI VOID *FscSetCacheSize = 0;
-NTSYSAPI VOID *KdDebuggerEnabled = 0;
-NTSYSAPI VOID *KdDebuggerNotPresent = 0;
-NTSYSAPI VOID *KfRaiseIrql = 0;
-NTSYSAPI VOID *KfLowerIrql = 0;
-NTSYSAPI VOID *KiBugCheckData = 0;
-NTSYSAPI VOID *KiUnlockDispatcherDatabase = 0;
-NTSYSAPI VOID *LaunchDataPage = 0;
-NTSYSAPI VOID *PhyGetLinkState = 0;
-NTSYSAPI VOID *PhyInitialize = 0;
-NTSYSAPI VOID *XboxEEPROMKey = 0;
-NTSYSAPI VOID *XboxHardwareInfo = 0;
-NTSYSAPI VOID *XboxHDKey = 0;
-NTSYSAPI VOID *XboxKrnlVersion = 0;
-NTSYSAPI VOID *XboxSignatureKey = 0;
-NTSYSAPI VOID *XeImageFileName = 0;
-NTSYSAPI VOID *XeLoadSection = 0;
-NTSYSAPI VOID *XeUnloadSection = 0;
-NTSYSAPI VOID *XcSHAInit = 0;
-NTSYSAPI VOID *XcSHAUpdate = 0;
-NTSYSAPI VOID *XcSHAFinal = 0;
-NTSYSAPI VOID *XcRC4Key = 0;
-NTSYSAPI VOID *XcRC4Crypt = 0;
-NTSYSAPI VOID *XcHMAC = 0;
-NTSYSAPI VOID *XcPKEncPublic = 0;
-NTSYSAPI VOID *XcPKDecPrivate = 0;
-NTSYSAPI VOID *XcPKGetKeyLen = 0;
-NTSYSAPI VOID *XcVerifyPKCS1Signature = 0;
-NTSYSAPI VOID *XcModExp = 0;
-NTSYSAPI VOID *XcDESKeyParity = 0;
-NTSYSAPI VOID *XcKeyTable = 0;
-NTSYSAPI VOID *XcBlockCrypt = 0;
-NTSYSAPI VOID *XcBlockCryptCBC = 0;
-NTSYSAPI VOID *XcCryptService = 0;
-NTSYSAPI VOID *XcUpdateCrypto = 0;
-NTSYSAPI VOID *XboxLANKey = 0;
-NTSYSAPI VOID *XboxAlternateSignatureKeys = 0;
-NTSYSAPI VOID *XePublicKeyData = 0;
-NTSYSAPI VOID *IdexChannelObject = 0;
-NTSYSAPI VOID *xsnprintf = 0;  // prefixed with "x" to avoid xlibc collisions
-NTSYSAPI VOID *xsprintf = 0;   // ""
-NTSYSAPI VOID *xvsnprintf = 0; // ""
-NTSYSAPI VOID *xvsprintf = 0;  // ""
-
 // ******************************************************************
 // * AvGetSavedDataAddress
 // ******************************************************************
