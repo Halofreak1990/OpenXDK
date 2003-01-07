334afa748267b2405d7c5799fa9ab59c6fec2678
caustik <caustik>
2003-01-07 21:35:40 +0000

    Preparing for 0.2.0 release.

--- include/xgfx2d/blit.h
+++ include/xgfx2d/blit.h
@@ -23,32 +23,32 @@ extern "C"
 #endif
 	
 
-//INSTRUCTIONS
-//************
-//pass in a blitter from blitters.h
-//dx,dy = destination x,y
-//sx,sy = source x,y
-//sw,sh = width and height of the copy
-//blitter = copying method, check blitters.h
-//parameter = if a blitter method needs a parameter pass it here,
-//            else pass 0
-//Sample:
-//blit(source,dest,10,10,0,0,20,20,alphavalue_blit,128)
-//will blit the topleft 20x20 of source to 10,10 in dest,
-//blended ~50%  (128/255)
+//  INSTRUCTIONS
+//  ************
+//  pass in a blitter from blitters.h
+//  dx,dy = destination x,y
+//  sx,sy = source x,y
+//  sw,sh = width and height of the copy
+//  blitter = copying method, check blitters.h
+//  parameter = if a blitter method needs a parameter pass it here,
+//              else pass 0
+//  Sample:
+//  blit(source,dest,10,10,0,0,20,20,alphavalue_blit,128)
+//  will blit the topleft 20x20 of source to 10,10 in dest,
+//  blended ~50%  (128/255)
 void blit(Bitmap *dest, Bitmap *src, 
 	int dx, int dy, 
 	int sx, int sy, int sw, int sh, 
 	void (*blitter)(uint32*,uint32*,int,int), 
 	int parameter);
 
-//just copies entire source bitmap to 0,0 of dest
+//  just copies entire source bitmap to 0,0 of dest
 void full_blit(Bitmap *dest, Bitmap *src, 
 	void (*blitter)(uint32*,uint32*,int,int), 
 	int parameter);
 
-//shortcut to copy entire source bitmap to a coordinate in dest
-//use for simple sprites
+//  shortcut to copy entire source bitmap to a coordinate in dest
+//  use for simple sprites
 void blit_at(Bitmap *dest, Bitmap *src, 
 	int x, int y, 
 	void (*blitter)(uint32*,uint32*,int,int), 
